5a21a022204bb76b9a37f386a73f1fb7b355e84f
mrazinshaikh <razinshaikh8732@gmail.com>
2023-03-19 11:35:06 +0530

    temp commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -29,5 +30,60 @@ class HomeController extends AbstractController
         // return new Response(
         //     '<html><body>Lucky number: '.$number.'</body></html>'
         // );
+    }
+
+    /**
+     * @Route("/request_check", name="request_check")
+     */
+    public function request_check()
+    {
+        return $this->render('check/check1.html.twig', [
+            'controller_name' => 'AuthController',
+        ]);
+    }
+
+    /**
+     * @Route("/request_check1", name="request_check1")
+     */
+    public function request_check1(LoggerInterface $logger)
+    {
+        ob_start();
+        ignore_user_abort(false);
+        sleep(3);
+
+        $logger->info('Request check 1');
+
+        ob_end_flush();
+        sleep(1);
+        $logger->info('Request check 2');
+
+        ob_end_flush();
+        sleep(1);
+        $logger->info('Request check 3');
+
+        ob_end_flush();
+        sleep(1);
+        $logger->info('Request check 4');
+
+        ob_end_flush();
+        sleep(1);
+        $logger->info('Request check 5');
+
+        ob_end_flush();
+        sleep(1);
+        $logger->info('Request check 6');
+
+        ob_end_flush();
+        sleep(1);
+        $logger->info('Request check 7');
+
+        ob_end_flush();
+        sleep(1);
+        $logger->info('Request check 8');
+        
+
+        return $this->json([
+            'message' => 'Request check 1',
+        ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
 
+        // $manager->flush();
+
+        $user = new User();
+        $user->setEmail('test@email.com');
+        $user->setPassword(
+            $this->encoder->hashPassword($user, '1234')
+        );
+        $user->setRoles(['ROLE_ADMIN']);
+        $manager->persist($user);
         $manager->flush();
     }
 }
